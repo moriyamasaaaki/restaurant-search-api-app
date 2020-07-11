ab96072c1d333ffcea536859c972ef199c731caf
moriyamasaaaki <moriya-7071@outlook.com>
2020-07-11 16:23:31 +0900

    refactor(index.js): warningの解消 #18

--- src/router/index.js
+++ src/router/index.js
@@ -8,29 +8,29 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
     path: "/about",
     name: "About",
-    component: () => import("../views/About.vue"),
+    component: () => import("../views/About.vue")
   },
   {
     path: "/restaurants",
     name: "RestaurantList",
-    component: () => import("../views/RestaurantList.vue"),
+    component: () => import("../views/RestaurantList.vue")
   },
 
   {
     path: "/restaurants/:restaurantId",
     name: "RestaurantDetail",
-    component: () => import("../views/RestaurantDetail.vue"),
+    component: () => import("../views/RestaurantDetail.vue")
   },
   {
     path: "*",
     name: "NotFound",
-    component: () => import("../views/errors/NotFound.vue"),
-  },
+    component: () => import("../views/errors/NotFound.vue")
+  }
 ];
 
 const router = new VueRouter({
