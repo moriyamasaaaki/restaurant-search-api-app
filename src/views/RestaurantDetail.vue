f609a386b610a304a682aeb5ff3839be2a0147cc
moriyamasaaaki <moriya-7071@outlook.com>
2020-07-14 21:02:00 +0900

    style(RestaurantDetail.vue): 詳細ページのUI修正 #16

--- src/views/RestaurantDetail.vue
+++ src/views/RestaurantDetail.vue
@@ -1,25 +1,67 @@
 <template>
   <div class="restaurantDetail">
-    <v-card>
-      <v-card-title>{{ restaurant.name }}</v-card-title>
-      <v-img
-        class="restaurantDetail__img"
-        v-if="!restaurant.image_url.shop_image1"
-        src="/img/unnamed.png"
-      />
-      <v-img
-        class="restaurantDetail__img"
-        v-else
-        :src="restaurant.image_url.shop_image1"
-      />
-      <v-card-text>{{ restaurant.pr.pr_long }}</v-card-text>
-      <v-chip-group column>
-        <v-chip>{{ restaurant.code.areaname_s }}</v-chip>
-        <v-chip>{{ restaurant.tel }}</v-chip>
-        <v-chip>{{ restaurant.opentime }}</v-chip>
-      </v-chip-group>
-      <a :href="restaurant.url">予約する</a>
-    </v-card>
+    <div>
+      <v-card-title class="restaurantDetail__title">
+        <span class="material-icons">restaurant</span>
+        {{ restaurant.name }}
+      </v-card-title>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-subtitle>
+            <div class="restaurantDetail__location">
+              <span class="material-icons">location_on</span>
+              {{ restaurant.code.areaname_s }}
+              /
+              {{ restaurant.access.line }}
+              {{ restaurant.access.station }}
+              {{ restaurant.access.station_exit }}
+              徒歩{{ restaurant.access.walk }}分
+            </div>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <div class="restaurantDetail__container">
+        <div class="restaurantDetail__img-list">
+          <v-img
+            class="restaurantDetail__img"
+            v-if="!restaurant.image_url.shop_image1"
+            src="/img/unnamed.png"
+          />
+          <v-img
+            class="restaurantDetail__img"
+            v-else
+            :src="restaurant.image_url.shop_image1"
+          />
+        </div>
+        <div class="restaurantDetail__content">
+          <v-card-text>{{ restaurant.pr.pr_long }}</v-card-text>
+          <v-divider class="mx-4"></v-divider>
+          <v-list class="transparent">
+            <v-list-item class="restaurantDetail__list">
+              <span class="material-icons">schedule</span>
+              <span>{{ restaurant.opentime }}</span>
+            </v-list-item>
+            <v-list-item class="restaurantDetail__list">
+              <span class="material-icons">settings_phone</span>
+              <span>{{ restaurant.tel }}</span>
+            </v-list-item>
+            <v-list-item class="restaurantDetail__list">
+              <span class="material-icons">money</span>
+              <span>平均予算: {{ restaurant.budget }}円</span>
+            </v-list-item>
+            <v-list-item class="restaurantDetail__list">
+              <span class="material-icons">warning</span>
+              <span>{{ restaurant.holiday }}</span>
+            </v-list-item>
+          </v-list>
+          <v-card-actions>
+            <a class="restaurantDetail__btn" :href="restaurant.url">
+              <v-btn class="ma-2" outlined color="indigo">予約する</v-btn>
+            </a>
+          </v-card-actions>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -74,10 +116,46 @@ h1 {
   font-size: 20px;
 }
 .restaurantDetail {
-  max-width: 500px;
+  max-width: 1000px;
   margin: 24px auto;
   &__img {
     width: 100%;
   }
+  &__container {
+    @include pc {
+      display: flex;
+      justify-content: center;
+      align-items: top;
+    }
+  }
+  &__img-list {
+    @include pc {
+      width: 40%;
+    }
+  }
+  &__content {
+    @include pc {
+      width: 55%;
+      padding: 0 8px;
+    }
+  }
+  &__list {
+    display: flex;
+    align-items: flex-start;
+  }
+  &__btn {
+    margin: 0 0 0 auto;
+    text-decoration: none;
+  }
+  &__title,
+  &__location {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.material-icons {
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 8px;
 }
 </style>
